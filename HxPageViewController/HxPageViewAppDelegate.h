e01400b42d0e93dc4ff362253fe3482bed2fe219
hxlich <q7h8d33j>
2014-06-11 10:26:31 -0400

    name checking

---
+++ HxPageViewController/HxPageViewAppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  KilnMonitorAppDelegate.h
+//  KilnMonitorPad
+//
+//  Created by Harry Xiao Han on 2014-05-07.
+//  Copyright (c) 2014 Suretrol Manufacturing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@interface HxPageViewAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HxPageViewController/HxPageViewController.m
@@ -0,0 +1,29 @@
+//
+//  hxswapViewController.m
+//  HxPageViewController
+//
+//  Created by Harry Xiao Han on 2014-06-11.
+//  Copyright (c) 2014 Harry Han. All rights reserved.
+//
+
+#import "HxPageViewController.h"
+
+@interface HxPageViewController ()
+
+@end
+
+@implementation HxPageViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
